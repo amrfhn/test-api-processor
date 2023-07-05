9e649913bdca975b1d80dad63885dc9ecfff941a
Amir Farhan bin Jefri <jefriaf@basfad.basf.net>
2023-07-06 00:53:10 +0800

    add delete

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -10,11 +10,10 @@ router.get('/', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
-
 })
 // Getting one
 // Creating one
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
     const rsvpList = new Rsvp({
         name: req.body.name,
         phoneNumber: req.body.phoneNumber,
@@ -28,9 +27,19 @@ router.post('/', async (req,res) => {
         res.status(201).json(newRsvp);
     } catch (error) {
         res.status(400).json({ message: error.message });
-    } 
+    }
 })
 // Updating one
 // Deleting one
+router.delete('/:id', (req, res) => {
+    Rsvp.findByIdAndDelete(req.params.id).then((rsvp) => {
+        if (!rsvp) {
+            return res.status(404).send();
+        }
+        res.send(rsvp);
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+})
 
 module.exports = router;
